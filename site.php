603fa9d1fad9c1a1d68dd78d9674b0a3f6af45df
zuzu01213 <lordzane345@gmail.com>
2023-10-30 04:55:51 -0400

    menambah file js dan mengubah file php

--- site.php
+++ site.php
@@ -206,3 +206,18 @@
     echo "same has {$same}cm laptop size <br>"
 ?> ---------------------------------------------------------------------------------------------------------->
 
+<?php 
+    $age = 0;
+
+    if ($age >= 18){
+        echo "You may enter this site";
+    } else if($age === 0) {
+        echo"You were just born";
+    } 
+    
+    else {
+        echo "You must be 18+ yo enter";
+    }
+
+
+?>
