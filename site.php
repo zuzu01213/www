3b99a1d42d88388eb4c314125d78340df53c571c
zuzu01213 <lordzane345@gmail.com>
2023-10-30 21:58:06 -0400

    mengubah php

--- site.php
+++ site.php
@@ -206,37 +206,98 @@
     echo "same has {$same}cm laptop size <br>"
 ?> ---------------------------------------------------------------------------------------------------------->
 
-<?php 
+<!--<?php 
         // IF AND ELSE STATEMENT
 
-    $age = 12;
+    $age = 66;
     $adult = true;
+    $pay = -2;
+    $weekly_pay = null;
+
+        // SALARY STATEMENT
+
+    if ($adult === true && $age >= 17 && $age <= 20 ) {
+        $weekly_pay = $pay;
+        echo "Your pay is: {$weekly_pay}$ <br>";
+    } 
+    else if ($adult === true && $age >= 21 && $age <= 30 ) {
+        $weekly_pay = $pay * 3;
+        echo "Your pay is: {$weekly_pay}$ <br>";
+    }  
+    else if ($adult === true && $age >= 31 && $age < 50 ) {
+        $weekly_pay = $pay * 5;
+        echo "Your pay is: {$weekly_pay}$ <br>";
+    }   
+    else if ($adult === true && $age >=50 && $pay >=0 ) {
+        $weekly_pay = $pay * 10;
+        echo "Because you are retirement, Your pay is: {$weekly_pay}$ <br>";
+    }
+    else if ($adult === true && $age >= 17 && $pay <0  ) {
+        $weekly_pay = $pay * 1000;
+        echo "Because you are lying, You deserve to get debt by total {$weekly_pay}$ <br>";
+    }  
+    else {
+        $weekly_pay = $pay * 0;
+        echo " Because you don't even have a job, Your pay is: {$weekly_pay}$ <br>"; 
+    }
+
+    
+        //  ADULT CONFIRMATION
+
     if ($adult === true && $age >= 17) {
         echo "You may enter <br>";
     } else {
         echo "You must be an adult to enter this site <br>";
     }
 
+        // AGE CONFIRMATION
+
     if ($age >= 17 && $age <=20 && $adult === true) {
         echo "I see you in that phase right now don't you?";
-    } else if($age >= 0 && $age <=5 && $adult === false) {
+    } 
+    else if($age >= 0 && $age <=5 && $adult === false) {
         echo"Please enter your age correctly";
-    } else if($age >= 100){
+    } 
+    else if($age >= 100){
         echo "aren't you to a little to old for this?";
-    } else if($age >= 6 && $age <= 13){
+    } 
+    else if($age >= 6 && $age <= 13){
         echo "Go way kiddo, you can't even produce semen right now.";
-    } else if($age >=20 && $age <= 29){
+    } 
+    else if($age >=20 && $age <= 29){
         echo "Man, this is your golden age don't waste your time on this shit.";
-    } else if($age >=30 && $age <= 35){
+    } 
+    else if($age >=30 && $age <= 35){
         echo "This is just sad, like really you are pathetic.";
-    } else if($age >= 35 && $age <=100){
+    } 
+    else if($age >= 35 && $age <=100){
         echo "Ugh, i wanna throw up just by looking at you!!!";
-    } else if($age <=0){
+    } 
+    else if($age <=0){
         echo "You haven't even born yet.";
-    }
-    else {
+    } else {
         echo "You must be 18+ yo enter";
+    }
+    
+
+?> -------------------------------------------------------------------------------------------------------->
+
+<?php 
+
+    // logical operations = combine conditional statements
+    // if(!condition1)
+
+    // && = True if both condition are true
+    // || = True if at least one condition is true
+    // ! = true if false. false if true.
+
+    $temp = 15;
+
+    if($temp < 0 || $temp > 30){
+        echo "The weather is good";
+    } 
+    else {
+        echo "The weather is bad";
     }
 
 ?>
-
