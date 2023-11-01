5faca037c49da3f4e7dc2fa2fc15e781332ed13e
zuzu01213 <lordzane345@gmail.com>
2023-11-01 03:19:32 -0400

    mengubah php

---
+++ include.php
@@ -0,0 +1,22 @@
+<?php 
+    //include() = Copies the content of a file (php/html/text)
+    // and includes it in your php file.
+    // sections of our website become reusable
+    // changes only need to be made in one place
+
+
+    include("about.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the home page <br>
+    Stuff about your home page can go here <br>
+</body>
+</html>
