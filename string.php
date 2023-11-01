7a81c19d086bff70f5f5fac0fb3d7f8138032928
zuzu01213 <lordzane345@gmail.com>
2023-11-01 01:25:10 -0400

    mengubah php

--- string.php
+++ string.php
@@ -1,5 +1,23 @@
 <?php 
+    $username = array("Keenan", "GOAT");
+    $phone = "123-456-789";
 
-
-
+    //$username = strtolower($username);
+    // $username = strtoupper($username);
+    // $username = trim($username);
+    // $username = str_pad($username, 15, " Rahmanda");
+    // $phone = str_replace("123-","666-", $phone);
+    // $username = strrev($username);
+    // $username = str_shuffle($username);
+    // $equals = strcmp($username, "KEENAN");
+    // $count = strlen($phone);
+    // $index = strpos($phone, "-");
+    // $first = substr($username, 0, 6);
+    // $last = substr($username, 1);
+    // $full = explode(" ", $username);
+    $username = implode(" ", $usernames);
+    
+    foreach ($full as $name){
+        echo $name . "<br>";
+    }
 ?>
